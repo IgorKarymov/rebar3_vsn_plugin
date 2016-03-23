a1826e1d53e97adfeb455679e80e12073838669c
Igor Karymov <ikarymov@unison.com>
2016-03-23 14:21:29 +0300

    vsn rewriter for releases

--- src/rebar3_vsn_plugin_prv.erl
+++ src/rebar3_vsn_plugin_prv.erl
@@ -31,7 +31,9 @@ do(State) ->
     rebar_api:info("Versions ~p",[Versions]),
     OverrideVsnForApps = proplists:get_value(override_vsn_for_apps, Options, []),
     State1 = rewrite_versions_for_apps(State, Versions, OverrideVsnForApps),
-    {ok, State1}.
+    OverrideVsnForRelease = proplists:get_value(override_vsn_for_release, Options),
+    State2 = rewrite_version_for_release(State1, Versions, OverrideVsnForRelease),
+    {ok, State2}.
 
 rewrite_versions_for_apps(State, Versions, OverrideVsnForApps) ->
     Apps = rebar_state:project_apps(State) ++ rebar_state:all_deps(State),
@@ -61,6 +63,55 @@ set_appfile_version(Filepath, NewVsn) when is_list(Filepath) ->
     ok = file:write_file(Filepath, Contents),
     rebar_api:info("Vsn changed from ~s --> ~s in: ~s",[Vsn, NewVsn, Filepath]),
     ok.
+
+rewrite_version_for_release(State, _Versions, undefined) -> State;
+rewrite_version_for_release(State, Versions, VsnRef) ->
+    NewRelVsn = proplists:get_value(VsnRef, Versions),
+    rebar_api:info("Setting release vsn in rebar.config to ~s", [NewRelVsn]),
+    set_rebar_relx_version("rebar.config", NewRelVsn),
+    State.
+
+set_rebar_relx_version(Filepath, NewVsn) ->
+    {ok, Bin} = file:read_file(Filepath),
+    Lines = lists:map(fun binary_to_list/1, binary:split(Bin, <<"\n">>, [global])),
+    case set_rebar_relx_version_1(NewVsn, Lines, false, []) of
+        {error, _} = E ->
+            E;
+        Contents ->
+            ok = file:write_file(Filepath, [strip(Contents), <<"\n">>])
+    end.
+
+set_rebar_relx_version_1(NewVsn, [Line | Lines], Found, Acc) ->
+    case string:str(Line, "release-version-marker") > 0 of
+        true ->
+            NewLine = io_lib:format("    \"~s\" %% release-version-marker", [NewVsn]),
+            set_rebar_relx_version_1(NewVsn, Lines, true, [NewLine|Acc]);
+        false ->
+            set_rebar_relx_version_1(NewVsn, Lines, Found, [Line|Acc])
+    end;
+set_rebar_relx_version_1(_, [], false, _Acc) ->
+    {error, relflow_marker_missing};
+set_rebar_relx_version_1(_, [], true, Acc) ->
+    [ [Line, <<"\n">>] || Line <- lists:reverse(Acc) ].
+
+strip(<<>>) ->
+    <<>>;
+strip(B) when is_list(B) ->
+    strip(iolist_to_binary(B));
+strip(B) when is_binary(B) ->
+    LastChar = binary:last(B),
+    case whitespace(LastChar) of
+        false ->
+            B;
+        true ->
+            strip(binary:part(B, 0, size(B)-1))
+    end.
+
+whitespace($ )  -> true;
+whitespace($\t) -> true;
+whitespace($\r) -> true;
+whitespace($\n) -> true;
+whitespace(_)   -> false.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
